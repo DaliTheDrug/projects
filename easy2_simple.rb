96558a1915215560011b294f2df92d66523cdd1a
Michael Marchand <michaeldavidmarchand@gmail.com>
2018-02-28 10:22:00 -0700

    Deleted some extra formulas

--- easy2_simple.rb
+++ easy2_simple.rb
@@ -5,9 +5,3 @@ puts "How much do you charge per player?"
 cost_per_player = $stdin.gets.chomp
 
 puts "Total revenue is: $#{total_players.to_i * cost_per_player.to_i} dollars."
-
-rev_from_forwards = forwards * cost_per_forward
-rev_from_defense = defense * cost_per_defense
-rev_from_goalies = goalies * cost_per_goalies
-
-variable_pricing_revenue = rev_from_forwards + rev_from_defense + rev_from_goalies
